a906f365395adb74d248ace64e80c6acef6c05e3
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-12 22:47:27 +0530

    first commit

---
+++ Array/mountain_array.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class mountain_array
+{
+    static public boolean validMountainArray(int[] arr) 
+    {
+        if(arr.length<3)
+        return false;
+
+        else
+        {
+            int max=0;
+            for(int i=0;i<arr.length;i++)
+            {
+                if(arr[i]>arr[max])
+                max=i;
+            }
+            if(max==arr.length-1 || max==0)
+            return false;
+            
+            int flag=1;
+            for(int i=1;i<max;i++)
+            {
+                if(arr[i-1]>=arr[i])
+                flag=0;
+            }
+            for(int i=max;i<arr.length-1;i++)
+            {
+                if(arr[i]<=arr[i+1])
+                flag=0;
+            }
+
+            if(flag==0)
+            return false;
+            else
+            return true;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter " + n + " elements : ");
+        for (int i = 0; i < n; i++)
+        arr[i]=sc.nextInt();
+
+        boolean check=validMountainArray(arr);
+        if(check)
+        System.out.println("It's a valid Mountain Array.");
+        else
+        System.out.println("It's not a valid Mountain Array.");
+        sc.close();     
+    }    
+}
