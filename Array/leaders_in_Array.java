86b7dcaebdfbe27f8453e1edeaa676722131ad9b
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-21 16:10:11 +0530

    first commit

--- Array/leaders_in_Array.java
+++ Array/leaders_in_Array.java
@@ -3,23 +3,38 @@ public class leaders_in_Array
 {
     static List<Integer> leaders(int nums[])
     {
-        //brute approach
+        //brute approach TC-->O(n^2)
         List<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length-1;i++)
+        /*for(int i=0;i<nums.length;i++)
         {
-            int check=nums[i];
             int flag=1;
             for(int j=i+1;j<nums.length;j++)
             {
-                if(nums[j]>check)
+                if(nums[j]>nums[i])
                 flag=0;
             }
-            if(flag==0)
-            continue;
-            else
-            list.add(check);
+            if(flag==1)
+            list.add(nums[i]);
         }
+        return list;*/
+
+        //optimal technique TC-->O(n)
+        int max=nums[nums.length-1];
+        list.add(max);
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            if(nums[i]>max)
+            {
+                max=nums[i];
+                list.add(max);
+            }
+        }
+        Collections.reverse(list); // to get the answer preserving the order
         return list;
+    }
+    static void reverse(List<Integer> list)
+    {
+
     }
     public static void main(String[] args) 
     {
