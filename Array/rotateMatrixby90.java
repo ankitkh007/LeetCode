193139d276e2ae7e4c9e77865f187ef363385193
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-22 23:05:12 +0530

    modified

--- Array/rotateMatrixby90.java
+++ Array/rotateMatrixby90.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class rotateMatrixby90 
 {
-    //my approach
+    /*//my approach
     static void rotate(int arr[][],int n)
     {
         int ans[][]=new int[n][n];
@@ -20,8 +20,41 @@ public class rotateMatrixby90
             for(int j=0;j<n;j++)
             System.out.print(ans[i][j]+" ");
             System.out.println();
+        }*/
+
+        //optimal approach
+        static void rotate(int arr[][],int n)
+        {
+            //here we find transpose
+            for(int i=0;i<n;i++)
+            {
+                for(int j=i;j<n;j++)
+                {
+                    int temp=arr[i][j];
+                    arr[i][j]=arr[j][i];
+                    arr[j][i]=temp;
+                }
+            }
+            //next we will reverse each row
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n/2;j++)
+                {
+                    int temp=arr[i][n-1-j];     //00--03, 01--02
+                    arr[i][n-1-j]=arr[i][j];
+                    arr[i][j]=temp;
+                }
+            }
+
+            //display final matrix
+            System.out.println("The matrix after rotation is : ");
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                System.out.print(arr[i][j]+" ");
+                System.out.println();
+            }
         }
-    }
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
