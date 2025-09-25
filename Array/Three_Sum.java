a39693fa693c8be91b921780d9fc1f8221823365
ankitkh007 <ankit123khanna456@gmail.com>
2025-09-25 22:53:43 +0530

    modified

--- Array/Three_Sum.java
+++ Array/Three_Sum.java
@@ -31,7 +31,7 @@ public class Three_Sum
         List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            if(i!=0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=nums.length-1;
             while(j<k)
@@ -51,8 +51,8 @@ public class Three_Sum
                     ans.add(list);
                     j++;
                     k--;
-                    while(j<k && nums[j]!=nums[j-1]) j++;
-                    while(j<k && nums[k]!=nums[k+1]) k++;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
