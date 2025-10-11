1b7fb42bdec0519ccf2bdd02f16e90ec170bf22d
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-11 23:55:36 +0530

    first commit

---
+++ Strings/binary_string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class binary_string {
+    static int binarySubstring(String s) {
+        // code here
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='1')
+            count++;
+        }
+        return (count*(count-1))/2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            System.out.println("Enter your String: ");
+            String str=sc.nextLine();
+    
+            System.out.println("The substrings of given String are: "+binarySubstring(str));
+    
+            sc.close();
+    }
+}
