3f13f135e29c20fb7e4ea0e7a021b80a8ab413e1
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-11 23:57:25 +0530

    first commit

--- Strings/binary_string.java
+++ Strings/binary_string.java
@@ -1,14 +1,28 @@
 import java.util.*;
 class binary_string {
     static int binarySubstring(String s) {
-        // code here
+        // brute
         int count=0;
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='1')
+            {
+                for(int j=i+1;j<s.length();j++)
+                {
+                    if(s.charAt(j)=='1')
+                    count++;
+                }
+            }
+        }
+        return count;
+
+        //optimal
+        /*for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='1')
             count++;
         }
-        return (count*(count-1))/2;
+        return (count*(count-1))/2;*/
     }
 
     public static void main(String[] args) {
