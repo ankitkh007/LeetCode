8ef15f9159ebd139faab22da02824667dac21c45
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-12 12:33:49 +0530

    first commit

--- Strings/panagram.java
+++ Strings/panagram.java
@@ -4,7 +4,7 @@ class panagram
     static final int MAX_CHAR=26;
     public static boolean checkPangram(String s) 
     {
-        boolean visited[]=new boolean[MAX_CHAR];
+        /*boolean visited[]=new boolean[MAX_CHAR];
         int n=s.length();
         
         for(int i=0;i<n;i++)
@@ -21,7 +21,19 @@ class panagram
             if(!visited[i])
             return false;
         }
-        return true;
+        return true;*/
+
+        //new approach
+        Set<Character> set=new HashSet<>();
+        int n=s.length();
+        s=s.toLowerCase();
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)>='a' && s.charAt(i)<='z')
+            set.add(s.charAt(i));
+        }
+        
+        return set.size()==26;
     }
 
     public static void main(String[] args) 
