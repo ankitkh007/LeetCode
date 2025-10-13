4c9b699cf23ffdcf0bf5a677a389aacd33464e81
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-13 09:27:26 +0530

    modified

--- Strings/anagram.java
+++ Strings/anagram.java
@@ -3,7 +3,7 @@ public class anagram
 {
     static boolean isAnagram(String s1,String s2)
     {
-        char arr1[]=s1.toCharArray();
+        /*char arr1[]=s1.toCharArray();
         char arr2[]=s2.toCharArray();
 
         Arrays.sort(arr1);
@@ -12,7 +12,27 @@ public class anagram
         if(Arrays.equals(arr1, arr2))
         return true;
         else
-        return false;
+        return false;*/
+
+        //HashMap approach
+        HashMap<Character, Integer> map=new HashMap<>();
+
+        //Traverse through 1st String and increment the frequency of each character
+        for(char ch:s1.toCharArray())
+        map.put(ch, map.getOrDefault(ch, 0)+1);
+
+        //Traverse through 2nd String and decrememt the frequency of each character
+        for(char ch:s2.toCharArray())
+        map.put(ch, map.getOrDefault(ch, 0)-1);
+
+        for(var ch: map.entrySet()) //if frequency of every character in the HashMap is zero then the Strings the Anagrams 
+        {
+            if(ch.getValue()!=0)
+            return false;
+        }
+
+        return true;
+
     }
     public static void main(String[] args) 
     {
