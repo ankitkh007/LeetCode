fe913be93f6d96b863c5498e4f36a7c476a0d56a
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-21 20:00:17 +0530

    first commit

---
+++ Binary Search/find_Nth_Root.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class find_Nth_Root
+{
+    static int getNthRoot(int x, int n)
+    {
+        int low=1,high=x;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int value=getPower(mid, n, x);
+            if(value==x)
+            return mid;
+            else if(value>x)
+            high=mid-1;
+            else
+            low=mid+1;
+        }
+        return -1;
+    }
+
+    static int getPower(int base, int power, int limit)
+    {
+        int ans=1;
+        for(int i=1;i<=power;i++)
+        {
+            ans*=base;
+            if(ans>limit) return limit+1; //if ans at any point while iteration exceeds our original limit(which is x) then simply break
+        }
+
+        return (int)ans;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int x=sc.nextInt();
+
+        System.out.println("Enter the value of n: ");
+        int n=sc.nextInt();
+
+        System.out.println("The square root of the number is: "+getNthRoot(x,n));
+        sc.close();
+    }
+}
