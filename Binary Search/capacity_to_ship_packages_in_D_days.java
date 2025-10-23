89146a69a1264b1872f2b8410a8d177ab41d3e96
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-23 20:21:59 +0530

    first commit

---
+++ Binary Search/capacity_to_ship_packages_in_D_days.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class capacity_to_ship_packages_in_D_days
+{
+    static int getMaxValue(int arr[]) //returns the maximum value from the array
+    {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        max=Math.max(max, arr[i]);
+
+        return max;
+    }
+
+    static int sumall(int arr[]) //calculates the sum of all the lements in the array
+    {
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        sum+=arr[i];
+
+        return sum;
+    }
+
+    static int TotalDaysRequired(int arr[], int cap)
+    {
+        int days=1,sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            // sum+=arr[i];
+            // if(sum>cap)
+            // {
+            //     days+=1;
+            //     sum=0;
+            // }
+
+            if(sum+arr[i]>cap)
+            {
+                days+=1;
+                sum=arr[i];
+            }
+            else
+            sum+=arr[i];
+        }
+
+        return days;
+    }
+    static int shipWithinDays(int[] weights, int days)
+    {
+        int low=getMaxValue(weights), high=sumall(weights);
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(TotalDaysRequired(weights, mid)<=days) //if total calculated days for any capacity is<=provided deadline
+            high=mid-1;                               //therefore everything after that will be possible so eliminate the right half
+            else
+            low=mid+1;
+        }
+        return low;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        System.out.println("Enter "+n+" elements:");
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+
+        System.out.println("Enter the maximum days for shipping: ");
+        int days=sc.nextInt();
+
+        System.out.println("The least weight capacity of the ship will be: "+ shipWithinDays(arr, days));
+        sc.close();
+    }
+}
