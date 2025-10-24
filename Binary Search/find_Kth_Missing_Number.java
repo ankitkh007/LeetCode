549c652f24042513f140101b2e4992db1ba782a6
ankitkh007 <ankit123khanna456@gmail.com>
2025-10-24 09:51:44 +0530

    modified

--- Binary Search/find_Kth_Missing_Number.java
+++ Binary Search/find_Kth_Missing_Number.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class find_Kth_Missing_Number
 {
-    static int findKthPositive(int[] arr, int k) 
+    //Brute approach
+    /*static int findKthPositive(int[] arr, int k) 
     {
         for(int i=0;i<arr.length;i++)
         {
@@ -10,6 +11,23 @@ public class find_Kth_Missing_Number
             break;
         }
         return k;
+    }*/
+
+    //BS approach
+    static int findKthPositive(int[] arr, int k)
+    {
+        int low=0,high=arr.length-1;
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int missing=arr[mid]-(mid+1);
+
+            if(missing<k) low=mid+1;
+            else high=mid-1;
+        }
+
+        return k+high+1;
     }
     public static void main(String[] args)
     {
