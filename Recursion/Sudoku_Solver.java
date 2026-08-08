cd0432e940fae46cbab7578af053ac99997a99fc
ankitkh007 <ankit123khanna456@gmail.com>
2026-08-08 13:25:11 +0530

    initial commit

--- Recursion/Sudoku_Solver.java
+++ Recursion/Sudoku_Solver.java
@@ -55,7 +55,7 @@ public class Sudoku_Solver {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j] + " ");
+                System.out.print(board[i][j] + "|");
             }
             System.out.println();
         }
